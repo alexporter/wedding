61e033b8a81bbfac62da126d7b433dc3f84a3fbd
thefamilyphysicians <familyphysicians.wix@gmail.com>
2015-11-15 08:42:23 -0700

    Most pages done

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $(this).scrollTop(0);
     $(window).attr('loaded', false);
     if(isIE())  $('body').addClass('is-ie');
-    
+
     $(window).load(function() {
         $('body').on('click', function(e) {
             var t = $(e.target);
@@ -13,7 +13,7 @@ $(document).ready(function() {
         });
         $('.main-container .page .tab').on('mouseover', function(e) {
             if ($('.page[preview]').length) return;
-            
+
             var page = $(e.target).parents('.page');
             if (page.attr('remove-active')) return;
             page.css({ top: ($('.main-container').outerHeight() - 60) + 'px' });
@@ -32,7 +32,7 @@ $(document).ready(function() {
             closePages();
             page.addClass('active');
         });
-        
+
         setTimeout(function() {
             $(this).attr('loaded', true);
             $('.loading-cover .logo').css({ display: 'block' }).addClass('fadeInUp');
@@ -40,7 +40,7 @@ $(document).ready(function() {
                 $('.loading-cover .logo').removeClass('fadeInUp').addClass('tada');
             }, 1000);
         }, 500);
-        
+
         setTimeout(function() {
             var numDots = Math.ceil($('.dots.left').outerWidth()/10); // Should be same for left or right
             addDots(0, numDots);
@@ -54,18 +54,36 @@ $(document).ready(function() {
 function isIE() {
     var ua = window.navigator.userAgent,
         msie = ua.indexOf('MSIE ');
-    
+
     return (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./));
 }
 
 function addDots(n, max) {
     if (n > max) return;
-    
+
     setTimeout(function() {
         $('.dots.left').html($('.dots.left').html() + '<div class="dot"></div>');
         $('.dots.right').html($('.dots.right').html() + '<div class="dot"></div>');
         addDots(n + 1, max);
     }, 10);
+}
+
+function showTopPage(page) {
+    $('.top-icons').css({ top: '-100px' });
+    $('.page .tab').css({ top: '0' });
+    $('.page .mountain').css({ top: '0' });
+    $('.top-page.' + page).addClass('active');
+}
+
+function closeTopPages() {
+    var activePages = $('.top-page.active');
+    activePages.removeClass('active');
+    setTimeout(function() {
+        $('.top-icons').css({ top: '10px' });
+        $('.page .mountain').css({ top: '-30px' });
+        $('.page.one .tab,.page.three .tab').css({ top: '-50px' });
+        $('.page.two .tab').css({ top: '-60px' });
+    }, 800);
 }
 
 function closePreviews() {
