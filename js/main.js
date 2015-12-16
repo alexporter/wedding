bbe488d03455bb4dae7828794561d089519e7cfd
alexporter <ajporter2011@gmail.com>
2015-12-15 21:49:33 -0700

    Added date countdown

--- js/main.js
+++ js/main.js
@@ -48,6 +48,9 @@ $(document).ready(function() {
                 $('.loading-cover').css({ top: '-100%' });
             }, (numDots * 10) + 1000);
         }, 1700);
+        
+        var deadline = new Date(2016, 6, 4);
+        initializeClock('countdown', deadline);
     });
 });
 
@@ -106,4 +109,43 @@ function closePages() {
 
 function goToPage(url) {
     window.open(url);
+}
+
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.now();
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+          clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
 }
